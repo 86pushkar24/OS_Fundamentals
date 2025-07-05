11cda1486b2c15823538dfadb58f4c52a8656f53
Pushkar Gupta <guptapushkar86@gmail.com>
2025-07-05 15:06:29 +0530

    executor

--- execv.c
+++ execv.c
@@ -5,10 +5,8 @@ int main()
 {
     char *args[] = {"/bin/ls", "-la", NULL};
     char *env[] = {NULL};
-
     execve("/bin/ls", args, env);
-
     // This line only runs if execve fails
     perror("execve failed");
     return 1;
-}
+} 
